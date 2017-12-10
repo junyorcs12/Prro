d03473c5660e63a6fc6a43617e094599e289ef36
junyorcs12 <junyorcs12@gmail.com>
2017-12-09 19:23:48 -0500

    Clase

---
+++ js/app.js
@@ -0,0 +1,35 @@
+/*alert("Hola Mundo")*/
+/*var nombre      = "Edison Rojas Villanera";
+const comunidad = "CabiHack";
+let taller      = "taller JS";
+
+var numComunidad    = comunidad.length;
+var notas           = 18;
+var num1            = document.getElementById("txt1");
+var num2            = document.getElementById("txt2");
+var btncalcular     = document.getElementById("btnCalcular");
+var resultado;
+
+console.log("La comunidad de "+comunidad+" estan dictando el "+taller+" por:"+nombre);
+
+if(notas < 10 ){
+    alert('Estoy desaprobado');
+}else if(notas <= 14){
+    alert('Estoy aprobado');
+}else{
+    alert('Mi vida si tiene sentido');
+}
+
+resultado = num1 * num2;
+console.log(resultado);
+
+
+
+btnCalcular.addEventListener("click",function(){
+    alert(num1);
+});
+
+function calcular (){
+    resultado = num1 * num2;
+}*/
+
